788e8c2007fa427518701bf64222d2319871903d
bzalyaliev <bzalyaliev@gmail.com>
2022-05-16 21:08:51 +0300

    Today's progress

--- src/javapuzzlers/LastLaugh.java
+++ src/javapuzzlers/LastLaugh.java
@@ -4,6 +4,22 @@ public class LastLaugh {
     public static void main(String args[]) {
         System.out.print("H" + "a");
         System.out.print('H' + 'a');
+
+        System.out.println();
+
+        //With StringBuffer
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append('H');
+        stringBuffer.append('a');
+        System.out.println(stringBuffer);
+
+        //Concatenation works only with String
+        System.out.println("" + 'H' + 'a');
+
+        //With String.valueOf
+        System.out.println(String.valueOf('H') + String.valueOf('a'));
+
+
     }
 }
 
