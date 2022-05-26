5e7b8ab905e1b770a83fc87bae43580b9da85dc9
bzalyaliev <bzalyaliev@gmail.com>
2022-05-26 11:09:00 +0300

    JavaAnagrams - works!

--- src/hackerrank/JavaAnagrams.java
+++ src/hackerrank/JavaAnagrams.java
@@ -1,11 +1,38 @@
 package hackerrank;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class JavaAnagrams {
+
     static boolean isAnagram(String a, String b) {
         // Complete the function
-        return a.contains(b);
+
+        boolean result = true;
+        Map<Character, Integer> mapForA = getCharacters(a);
+        Map<Character, Integer> mapForB = getCharacters(b);
+
+        /*[a: 1, b: 2]
+          [h: 2, z: 1]
+        */
+        for (Map.Entry<Character, Integer> entry : mapForA.entrySet()) {
+            result = mapForB.get(entry.getKey()).equals(entry.getValue());
+        }
+        return result;
+    }
+
+    private static Map<Character, Integer> getCharacters(String a) {
+        char[] characters = a.toCharArray();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (Character character : characters) {
+            // если уже есть ключ, то прибавляем единицу
+            if (map.containsKey(character)) {
+                map.put(character, map.get(character) + 1);
+                // Если нет, то кладем ключ и присваиваем значение 1
+            } else {
+                map.put(character, 1);
+            }
+        }
+        return map;
     }
 
     public static void main(String[] args) {
